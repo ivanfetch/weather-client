76434612675f5521b32183bdb052c97ae9f26612
Ivan Fetch <ivan@ivanfetch.com>
2021-04-13 18:03:30 -0400

    Fix and improve the test for `formAPIURL`

--- weather_internal_test.go
+++ weather_internal_test.go
@@ -1,27 +1,27 @@
 package weather
 
 import (
-	"fmt"
-	"github.com/google/go-cmp/cmp"
-	"net/url"
 	"testing"
 )
 
-// A non-working weather API key
-const testApiKey = "abc123xyz"
-
-// A city used in multiple tests.
-const testCity = "Great Neck Plaza,NY,US"
-
 func TestFormAPIUrl(t *testing.T) {
+	// THis may turn into a table-test to test multiple units,
+	// if this test ends up being kept.
+	const testCity = "Great Neck Plaza,NY,US"
+	const testUnits = "f"
+
 	t.Parallel()
 
-	client := NewClient(testApiKey)
+	client := NewClient("DummyAPIKey")
 
-	want := fmt.Sprintf("https://%s%s/?q=%s&appid=%s%s", client.ApiHost, client.ApiUri, url.QueryEscape(testCity), testApiKey, client.ApiQueryOptions)
-	got := client.formAPIUrl(testCity)
+	want := "https://api.openweathermap.org/data/2.5/forecast/?q=Great+Neck+Plaza%2CNY%2CUS&appid=DummyAPIKey&units=imperial&cnt=1"
+	got, err := client.formAPIUrl(testCity, testUnits)
+
+	if err != nil {
+		t.Errorf("Errorwhile testing formAPIUrl(%s, %s): %v\n", testCity, testUnits, err)
+	}
 
 	if want != got {
-		t.Errorf("Want %q, got %q, testing formUrl(%s, %s)\n", want, got, testApiKey, testCity)
+		t.Errorf("Want %q, got %q, testing formAPIUrl(%s, %s)\n", want, got, testCity, testUnits)
 	}
 }
