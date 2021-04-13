f0fa5f1a596bf0d559e71df722921c539da2975a
Ivan Fetch <ivan@ivanfetch.com>
2021-04-13 18:03:30 -0400

    Add `city` flag with an environment variable override, other small fixes

--- cmd/main.go
+++ cmd/main.go
@@ -1,37 +1,46 @@
 // The weather CLI returns a brief weather forecast.
-// TODO: use the cobra package
-// to provide usage and nicer environment variable and command-line options.
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"weather"
 )
 
 func main() {
-	apiKey := os.Getenv("WEATHERCASTER_API_KEY")
+	apiKey := os.Getenv("OPENWEATHERMAP_API_KEY")
 	if apiKey == "" {
-		fmt.Fprintf(os.Stderr, "Please set the WEATHERCASTER_API_KEY environment variable to an OpenWeatherMap API key.\n")
+		fmt.Fprintf(os.Stderr, "Please set the OPENWEATHERMAP_API_KEY environment variable to an OpenWeatherMap API key.\n")
 		fmt.Fprintf(os.Stderr, "To obtain an API key, see https://home.openweathermap.org/api_keys\n")
 		os.Exit(1)
 	}
 
-	city := os.Getenv("WEATHERCASTER_CITY")
-	if city == "" {
-		city = "Great Neck Plaza,NY,US"
-		fmt.Printf("Defaulting city to %s - to override, please set the WEATHERCASTER_CITY environment variable.\n", city)
+	var city *string
+	city = flag.String("city", "", `The name of the city  to get a weather forecast. A city can be specified as:
+	"CityName"
+	"CityName,StateName"
+	"CityName,StateName,CountryCode"
+	For example: "Great Neck Plaza,NY,US"
+`)
+
+	flag.Parse()
+	if *city == "" {
+		*city = os.Getenv("WEATHERCASTER_CITY")
+	}
+	if *city == "" {
+		fmt.Println("Please specify a city using either the -city command-line flag, or by setting the WEATHERCASTER_CITY environment variable.")
+		os.Exit(1)
 	}
 
 	wc := weather.NewClient(apiKey)
 
-	forecast, err := wc.ForecastByCity(city)
+	forecast, err := wc.Forecast(*city)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
 	fmt.Printf("The brief weather forecast is %s\n", forecast)
 
-	fmt.Printf("The full API response is: %+v\n", wc.GetAPIResponse())
 }
